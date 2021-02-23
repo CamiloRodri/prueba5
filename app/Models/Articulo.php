4ef344a616bd6a8a74ee938d824cee5e2af5031d
Camilo Rodriguez <camilorodri28@outlook.com>
2021-02-23 09:59:38 -0500

    Modelos

--- app/Models/Articulo.php
+++ app/Models/Articulo.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Articulo extends Model
 {
     use HasFactory;
+
+    protected $table = 'articulo';
+
+    protected $primaryKey = 'id';
+
+    public function categoria(){
+        return $this->belongsTo('App\Models\Categoria', 'categoria_id');
+    }
 }
