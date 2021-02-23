cf3996aadb4fd4667cc44e1ce498fb505902ac13
Camilo Rodriguez <camilorodri28@outlook.com>
2021-02-23 10:35:21 -0500

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+* Categoria
+*/
+Route::get('/categoriaver', 'App\Http\Controllers\CategoriaController@index');
+Route::post('/categoriacrear', 'App\Http\Controllers\CategoriaController@store');
+Route::put('/categoriamodificar', 'App\Http\Controllers\CategoriaController@update');
+Route::delete('/categoriaeliminar', 'App\Http\Controllers\CategoriaController@destroy');
+
+
+/*
+* Articulo
+*/
+Route::get('/articulover', 'App\Http\Controllers\ArticuloController@index');
+Route::post('/articulocrear', 'App\Http\Controllers\ArticuloController@store');
+Route::put('/articulomodificar', 'App\Http\Controllers\ArticuloController@update');
+Route::delete('/articuloeliminar', 'App\Http\Controllers\ArticuloController@destroy');
